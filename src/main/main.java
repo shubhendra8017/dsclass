fe7db5f4556b2f1b0f44c3d17d41ee5deec9cca7
shubhendra <shubhendrapandey80@outlook.com>
2019-01-11 15:16:59 +0530

    hello world

--- src/main/main.java
+++ src/main/main.java
@@ -1,9 +1,20 @@
 package main;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("hello world");
-        System.out.printf("another line");
+        System.out.println("another line");
+        BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("enter yor name");
+        String name=bufferedReader.readLine();
+        System.out.println(name);
+        StringBuffer word= new StringBuffer(name);
+        word.setCharAt(0,'m');
+        System.out.println(word);
+
+
     }
 }
 
